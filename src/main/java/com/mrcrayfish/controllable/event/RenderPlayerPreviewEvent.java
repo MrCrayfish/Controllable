bfe214aecbaabc4d9b41f28c342ee56cdfdea968
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-13 22:27:34 +1030

    Fixed RenderPlayerPreviewEvent not being cancelable

--- src/main/java/com/mrcrayfish/controllable/event/RenderPlayerPreviewEvent.java
+++ src/main/java/com/mrcrayfish/controllable/event/RenderPlayerPreviewEvent.java
@@ -1,10 +1,12 @@
 package com.mrcrayfish.controllable.event;
 
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 /**
  * Author: MrCrayfish
  */
+@Cancelable
 public class RenderPlayerPreviewEvent extends Event
 {
 }
