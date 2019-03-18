74eee3fae49758ee830dc0ee09c4b48ab847d555
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-19 00:32:11 +1030

    Updated version

--- src/main/java/com/mrcrayfish/controllable/Reference.java
+++ src/main/java/com/mrcrayfish/controllable/Reference.java
@@ -7,6 +7,6 @@ public class Reference
 {
     public static final String MOD_ID = "controllable";
     public static final String MOD_NAME = "Controllable";
-    public static final String MOD_VERSION = "0.1.1";
+    public static final String MOD_VERSION = "0.1.2";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
 }
