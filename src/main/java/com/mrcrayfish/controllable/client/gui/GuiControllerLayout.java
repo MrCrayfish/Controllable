f7d180b2b3087f689d031b1e79503e3312479e07
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-20 02:41:25 +1030

    Fixed pressed thumb sticks not rendering correctly

--- src/main/java/com/mrcrayfish/controllable/client/gui/GuiControllerLayout.java
+++ src/main/java/com/mrcrayfish/controllable/client/gui/GuiControllerLayout.java
@@ -21,23 +21,23 @@ public class GuiControllerLayout extends GuiScreen
     @Override
     public void initGui()
     {
-        controllerButtons.add(new ControllerButton(Buttons.A, 29, 11, 7, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.B, 32, 8, 13, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.X, 26, 8, 16, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.Y, 29, 5, 10, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.LEFT_BUMPER, 5, 0, 25, 0, 7, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.RIGHT_BUMPER, 26, 0, 32, 0, 7, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.LEFT_TRIGGER, 5, -8, 39, 0, 7, 6, 5));
-        controllerButtons.add(new ControllerButton(Buttons.RIGHT_TRIGGER, 26, -8, 39, 0, 7, 6, 5));
-        controllerButtons.add(new ControllerButton(Buttons.DPAD_DOWN, 6, 11, 19, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.DPAD_RIGHT, 9, 8, 19, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.DPAD_LEFT, 3, 8, 19, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.DPAD_UP, 6, 5, 19, 0, 3, 3, 5));
-        controllerButtons.add(new ControllerButton(Buttons.SELECT, 14, 6, 22, 0, 3, 2, 5));
-        controllerButtons.add(new ControllerButton(Buttons.START, 21, 6, 22, 0, 3, 2, 5));
-        controllerButtons.add(new ControllerButton(Buttons.HOME, 17, 10, 46, 0, 4, 4, 5));
-        controllerButtons.add(new ControllerAxis(Buttons.LEFT_THUMB_STICK, 9, 14, 0, 0, 7, 7, 5));
-        controllerButtons.add(new ControllerAxis(Buttons.RIGHT_THUMB_STICK, 22, 14, 0, 0, 7, 7, 5));
+        controllerButtons.add(new ControllerButton(Buttons.A, 29, 9, 7, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.B, 32, 6, 13, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.X, 26, 6, 16, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.Y, 29, 3, 10, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.LEFT_BUMPER, 5, -2, 25, 0, 7, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.RIGHT_BUMPER, 26, -2, 32, 0, 7, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.LEFT_TRIGGER, 5, -10, 39, 0, 7, 6, 5));
+        controllerButtons.add(new ControllerButton(Buttons.RIGHT_TRIGGER, 26, -10, 39, 0, 7, 6, 5));
+        controllerButtons.add(new ControllerButton(Buttons.DPAD_DOWN, 6, 9, 19, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.DPAD_RIGHT, 9, 6, 19, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.DPAD_LEFT, 3, 6, 19, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.DPAD_UP, 6, 3, 19, 0, 3, 3, 5));
+        controllerButtons.add(new ControllerButton(Buttons.SELECT, 14, 4, 22, 0, 3, 2, 5));
+        controllerButtons.add(new ControllerButton(Buttons.START, 21, 4, 22, 0, 3, 2, 5));
+        controllerButtons.add(new ControllerButton(Buttons.HOME, 17, 8, 46, 0, 4, 4, 5));
+        controllerButtons.add(new ControllerAxis(Buttons.LEFT_THUMB_STICK, 9, 12, 0, 0, 7, 7, 5));
+        controllerButtons.add(new ControllerAxis(Buttons.RIGHT_THUMB_STICK, 22, 12, 0, 0, 7, 7, 5));
     }
 
     @Override
@@ -50,7 +50,7 @@ public class GuiControllerLayout extends GuiScreen
         int height = 29 * 5;
         int x = this.width / 2 - width / 2;
         int y = this.height / 2 - 50;
-        drawScaledCustomSizeModalRect(x, y, 0, 14, 38, 29, width, height, 256, 256);
+        drawScaledCustomSizeModalRect(x, y, 50, 0, 38, 29, width, height, 256, 256);
         GlStateManager.disableBlend();
         controllerButtons.forEach(controllerButton -> controllerButton.draw(x, y, mouseX, mouseY));
     }
