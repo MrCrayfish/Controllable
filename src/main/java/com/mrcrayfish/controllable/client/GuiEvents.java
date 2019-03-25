443111e46063735fe0a3ca627c6bd2876ee3bed6
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-25 23:21:42 +1030

    Controller selection is now found in options

--- src/main/java/com/mrcrayfish/controllable/client/GuiEvents.java
+++ src/main/java/com/mrcrayfish/controllable/client/GuiEvents.java
@@ -7,6 +7,7 @@ import com.studiohartman.jamepad.ControllerManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiOptions;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -32,33 +33,21 @@ public class GuiEvents
             controller.resetButtonStates();
         }
 
-        if(event.getGui() instanceof GuiMainMenu)
+        if(event.getGui() instanceof GuiOptions)
         {
-            int y = (event.getGui().height / 4 + 48) + 24 * 2;
-            event.getButtonList().add(new GuiButtonController(6969, (event.getGui().width / 2) - 124, y));
-        }
-        else if(event.getGui() instanceof GuiIngameMenu)
-        {
-            int y = (event.getGui().height / 4 + 32) + 24 * 2;
-            event.getButtonList().add(new GuiButtonController(6969, (event.getGui().width / 2) - 124, y));
+            int y = event.getGui().height / 6 + 72 - 6;
+            event.getButtonList().add(new GuiButtonController(6969, (event.getGui().width / 2) + 5 + 150 + 4, y));
         }
     }
 
     @SubscribeEvent
     public void onAction(GuiScreenEvent.ActionPerformedEvent event)
     {
-        if(event.getGui() instanceof GuiMainMenu)
+        if(event.getGui() instanceof GuiOptions)
         {
             if(event.getButton().id == 6969)
             {
                 Minecraft.getMinecraft().displayGuiScreen(new GuiControllerSelection(manager, true));
-            }
-        }
-        else if(event.getGui() instanceof GuiIngameMenu)
-        {
-            if(event.getButton().id == 6969)
-            {
-                Minecraft.getMinecraft().displayGuiScreen(new GuiControllerSelection(manager, false));
             }
         }
     }
