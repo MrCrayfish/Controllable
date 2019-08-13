f543f4558c60ffcde152960ab5c1b4b128458ebc
SuperKael <kaelhaney@gmail.com>
2019-08-13 16:57:39 -0500

    Updated version number

--- src/main/java/com/mrcrayfish/controllable/Reference.java
+++ src/main/java/com/mrcrayfish/controllable/Reference.java
@@ -7,5 +7,5 @@ public class Reference
 {
     public static final String MOD_ID = "controllable";
     public static final String MOD_NAME = "Controllable";
-    public static final String MOD_VERSION = "0.3.1";
+    public static final String MOD_VERSION = "0.4.0";
 }
