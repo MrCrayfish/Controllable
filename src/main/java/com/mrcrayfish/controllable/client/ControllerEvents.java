b67eea009252b97dab3d968bcd30d74d00a5b1ca
MrCrayfish <mrcrayfish@hotmail.com>
2019-11-11 16:31:20 +1030

    Added vibration when using items

--- src/main/java/com/mrcrayfish/controllable/client/ControllerEvents.java
+++ src/main/java/com/mrcrayfish/controllable/client/ControllerEvents.java
@@ -2,7 +2,10 @@ package com.mrcrayfish.controllable.client;
 
 import com.mrcrayfish.controllable.Controllable;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.UseAction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /**
@@ -11,6 +14,42 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class ControllerEvents
 {
     private float prevHealth = -1;
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void onPlayerUsingItem(LivingEntityUseItemEvent.Tick event)
+    {
+        if(!Controllable.getOptions().useForceFeedback())
+        {
+            return;
+        }
+
+        Controller controller = Controllable.getController();
+        if(controller != null)
+        {
+            float magnitudeFactor = 0.5F;
+            UseAction action = event.getItem().getUseAction();
+            switch(action)
+            {
+                case EAT:
+                case DRINK:
+                    magnitudeFactor = 0.5F;
+                    break;
+                case BLOCK:
+                    magnitudeFactor = 0.25F;
+                    break;
+                case SPEAR:
+                    magnitudeFactor = MathHelper.clamp((event.getItem().getUseDuration() - event.getDuration()) / 20F, 0.0F, 0.25F) / 0.25F;
+                    break;
+                case BOW:
+                    magnitudeFactor = MathHelper.clamp((event.getItem().getUseDuration() - event.getDuration()) / 20F, 0.0F, 1.0F) / 1.0F;
+                    break;
+                case CROSSBOW:
+                    magnitudeFactor = MathHelper.clamp((event.getItem().getUseDuration() - event.getDuration()) / 20F, 0.0F, 1.5F) / 1.5F;
+                    break;
+            }
+            controller.getSDL2Controller().rumble(0.5F * magnitudeFactor, 0.5F * magnitudeFactor, 50);
+        }
+    }
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent event)
