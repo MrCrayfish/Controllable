4fd7a01e06339ac350cfcac42f80fefb719e80af
MrCrayfish <mrcrayfish@hotmail.com>
2021-01-26 03:55:06 +1030

    Fixed mixin crash when using Optifine

---
+++ src/main/java/com/mrcrayfish/controllable/mixin/ControllableMixinPlugin.java
@@ -0,0 +1,66 @@
+package com.mrcrayfish.controllable.mixin;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Author: MrCrayfish
+ */
+public class ControllableMixinPlugin implements IMixinConfigPlugin
+{
+    private boolean optifineLoaded;
+
+    @Override
+    public void onLoad(String mixinPackage)
+    {
+        try
+        {
+            Class.forName("optifine.Installer");
+            this.optifineLoaded = true;
+        }
+        catch (ClassNotFoundException e)
+        {
+            this.optifineLoaded = false;
+        }
+    }
+
+    @Override
+    public String getRefMapperConfig()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName)
+    {
+        return this.optifineLoaded ? !mixinClassName.equals("com.mrcrayfish.controllable.mixin.client.GameRendererMixin") : !mixinClassName.equals("com.mrcrayfish.controllable.mixin.client.OptifineGameRendererMixin");
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets)
+    {
+
+    }
+
+    @Override
+    public List<String> getMixins()
+    {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo)
+    {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo)
+    {
+
+    }
+}
---
+++ src/main/java/com/mrcrayfish/controllable/mixin/client/OptifineGameRendererMixin.java
@@ -0,0 +1,37 @@
+package com.mrcrayfish.controllable.mixin.client;
+
+import com.mrcrayfish.controllable.Config;
+import com.mrcrayfish.controllable.Controllable;
+import com.mrcrayfish.controllable.client.ControllerInput;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mixin(GameRenderer.class)
+public class OptifineGameRendererMixin
+{
+    /**
+     * Fixes the mouse position when virtual mouse is turned on for controllers.
+     */
+    @ModifyArgs(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/optifine/reflect/Reflector;callVoid(Lnet/optifine/reflect/ReflectorMethod;[Ljava/lang/Object;)V", remap = false))
+    private void drawScreen(Args args)
+    {
+        Object[] params = args.get(1);
+        ControllerInput input = Controllable.getInput();
+        if(Controllable.getController() != null && Config.CLIENT.options.virtualMouse.get() && input.getLastUse() > 0)
+        {
+            Minecraft minecraft = Minecraft.getInstance();
+            int mouseX = (int) (input.getVirtualMouseX() * (double) minecraft.getMainWindow().getScaledWidth() / (double) minecraft.getMainWindow().getWidth());
+            int mouseY = (int) (input.getVirtualMouseY() * (double) minecraft.getMainWindow().getScaledHeight() / (double) minecraft.getMainWindow().getHeight());
+            params[2] = mouseX;
+            params[3] = mouseY;
+        }
+    }
+}
