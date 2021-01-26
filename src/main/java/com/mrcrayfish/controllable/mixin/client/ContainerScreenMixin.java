f029e2fa020d79d07e6fd1a337961be5fc38397f
MrCrayfish <mrcrayfish@hotmail.com>
2021-01-27 01:25:03 +1030

    Fixed handleMouseClick calling itself causing the game to crash

--- src/main/java/com/mrcrayfish/controllable/mixin/client/ContainerScreenMixin.java
+++ src/main/java/com/mrcrayfish/controllable/mixin/client/ContainerScreenMixin.java
@@ -1,18 +1,20 @@
 package com.mrcrayfish.controllable.mixin.client;
 
 import com.mrcrayfish.controllable.Controllable;
-import com.mrcrayfish.controllable.client.ButtonBindings;
 import com.mrcrayfish.controllable.client.Buttons;
 import com.mrcrayfish.controllable.client.Controller;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.inventory.container.ClickType;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Author: MrCrayfish
@@ -23,9 +25,6 @@ public abstract class ContainerScreenMixin
     @Shadow
     private ItemStack shiftClickedSlot;
 
-    @Invoker(value = "handleMouseClick")
-    abstract void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type);
-
     @Redirect(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/inventory/ContainerScreen;handleMouseClick(Lnet/minecraft/inventory/container/Slot;IILnet/minecraft/inventory/container/ClickType;)V", ordinal = 1))
     private void onClicked(ContainerScreen screen, Slot slotIn, int slotId, int mouseButton, ClickType type)
     {
@@ -34,7 +33,7 @@ public abstract class ContainerScreenMixin
             this.shiftClickedSlot = slotIn != null && slotIn.getHasStack() ? slotIn.getStack().copy() : ItemStack.EMPTY;
             type = ClickType.QUICK_MOVE;
         }
-        this.handleMouseClick(slotIn, slotId, mouseButton, type);
+        this.handleMouseClick(screen, slotIn, slotId, mouseButton, type);
     }
 
     @Redirect(method = "mouseReleased", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/inventory/ContainerScreen;handleMouseClick(Lnet/minecraft/inventory/container/Slot;IILnet/minecraft/inventory/container/ClickType;)V", ordinal = 9))
@@ -45,7 +44,31 @@ public abstract class ContainerScreenMixin
             this.shiftClickedSlot = slotIn != null && slotIn.getHasStack() ? slotIn.getStack().copy() : ItemStack.EMPTY;
             type = ClickType.QUICK_MOVE;
         }
-        this.handleMouseClick(slotIn, slotId, mouseButton, type);
+        this.handleMouseClick(screen, slotIn, slotId, mouseButton, type);
+    }
+
+    private Method method;
+
+    /**
+     * Manually invokes handleMouseClick from ContainerScreen using Java reflection. Mixins does not
+     * support the ability to invoke methods from sub classes when using Invoker annotation. If I am
+     * doing something wrong with Invoker, please PR into my repo!
+     */
+    private void handleMouseClick(ContainerScreen screen, Slot slotIn, int slotId, int mouseButton, ClickType type)
+    {
+        // Cache the method as it only needs to be found once.
+        if(this.method == null)
+        {
+            this.method = ObfuscationReflectionHelper.findMethod(ContainerScreen.class, "func_184098_a", Slot.class, int.class, int.class, ClickType.class);
+        }
+        try
+        {
+            this.method.invoke(screen, slotIn, slotId, mouseButton, type);
+        }
+        catch(IllegalAccessException | InvocationTargetException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     /**
