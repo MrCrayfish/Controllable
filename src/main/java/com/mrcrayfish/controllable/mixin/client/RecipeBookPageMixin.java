7b27605be9aee0fec78442ad745b6570f04c7d8a
MrCrayfish <mrcrayfish@hotmail.com>
2023-03-18 12:22:17 +1030

    📦 Added "View More" tooltip to craft book

--- src/main/java/com/mrcrayfish/controllable/mixin/client/RecipeBookPageMixin.java
+++ src/main/java/com/mrcrayfish/controllable/mixin/client/RecipeBookPageMixin.java
@@ -2,11 +2,13 @@ package com.mrcrayfish.controllable.mixin.client;
 
 import com.mrcrayfish.controllable.Config;
 import com.mrcrayfish.controllable.Controllable;
+import com.mrcrayfish.controllable.client.ButtonBindings;
 import com.mrcrayfish.controllable.client.Buttons;
 import com.mrcrayfish.controllable.client.util.ClientHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookPage;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -24,7 +26,11 @@ public class RecipeBookPageMixin
     {
         if(Controllable.getInput().isControllerInUse() && Config.CLIENT.options.quickCraft.get())
         {
-            components.add(Component.translatable("controllable.tooltip.craft", ClientHelper.getButtonComponent(Buttons.A)).withStyle(ChatFormatting.YELLOW));
+            if(components.removeIf(c -> c.getContents() instanceof TranslatableContents t && t.getKey().equals("gui.recipebook.moreRecipes")))
+            {
+                components.add(Component.translatable("controllable.tooltip.more_recipes", ClientHelper.getButtonComponent(ButtonBindings.SPLIT_STACK.getButton())).withStyle(ChatFormatting.YELLOW));
+            }
+            components.add(Component.translatable("controllable.tooltip.craft", ClientHelper.getButtonComponent(ButtonBindings.PICKUP_ITEM.getButton())).withStyle(ChatFormatting.YELLOW));
         }
         return components;
     }
