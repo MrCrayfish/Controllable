3c5f7546acb59630b0dd64ee74409eb3a7a13ae4
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-01-21 17:27:04 +1030

    📦 Background is now blurred when listening for input

--- common/src/main/java/com/mrcrayfish/controllable/client/gui/screens/SettingsScreen.java
+++ common/src/main/java/com/mrcrayfish/controllable/client/gui/screens/SettingsScreen.java
@@ -145,10 +145,10 @@ public class SettingsScreen extends Screen
         super.render(graphics, !waitingForInput ? mouseX : -1, !waitingForInput ? mouseY : -1, partialTick);
         if(waitingForInput)
         {
+            this.renderBlurredBackground(partialTick);
             PoseStack stack = graphics.pose();
             stack.pushPose();
             stack.translate(0, 0, 100);
-            RenderSystem.disableDepthTest();
             graphics.fillGradient(0, 0, this.width, this.height, 0xE0101010, 0xF0101010);
             ScreenHelper.drawRoundedBox(graphics, (int) (this.width * 0.125), this.height / 4, (int) (this.width * 0.75), this.height / 2, 0x99000000);
             Component pressButtonLabel = Component.translatable("controllable.gui.waiting_for_input").withStyle(ChatFormatting.YELLOW);
@@ -156,7 +156,6 @@ public class SettingsScreen extends Screen
             Component time = Component.literal(Integer.toString((int) Math.ceil(this.remainingTime / 20.0)));
             Component inputCancelLabel = Component.translatable("controllable.gui.input_cancel", time);
             graphics.drawCenteredString(this.font, inputCancelLabel, this.width / 2, this.height / 2 + 3, 0xFFFFFFFF);
-            RenderSystem.enableDepthTest();
             stack.popPose();
         }
     }
