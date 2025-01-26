08e11c079784dec967e790166712a272f28f463f
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-01-27 02:19:09 +1030

    📦 Added link icon hint when selecting controllers

--- common/src/main/java/com/mrcrayfish/controllable/client/gui/components/ControllerList.java
+++ common/src/main/java/com/mrcrayfish/controllable/client/gui/components/ControllerList.java
@@ -216,6 +216,10 @@ public class ControllerList extends TabSelectionList<ControllerList.ControllerEn
                 ScreenHelper.drawRoundedBox(graphics, left, top, listWidth, slotHeight, 0xFF000000);
                 graphics.blitSprite(CHECKMARK, left + 2, top, 18, 18);
             }
+            else if(Controllable.getController() != null && hovered)
+            {
+                graphics.blit(Icons.TEXTURE, left + 4, top + 4, 11, 11, 110, 0, 11, 11, Icons.TEXTURE_WIDTH, Icons.TEXTURE_HEIGHT);
+            }
             Font font = Minecraft.getInstance().font;
             graphics.drawString(font, this.label, left + 22, top + (slotHeight - font.lineHeight) / 2 + 1, 0xFFFFFF);
         }
