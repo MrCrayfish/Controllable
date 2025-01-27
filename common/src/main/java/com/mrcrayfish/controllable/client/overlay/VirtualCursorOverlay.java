1ece67483e78ee63a20e85d7c1036d527e45055c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-01-27 13:23:19 +1030

    📦 Restore hovering slot cursor

--- common/src/main/java/com/mrcrayfish/controllable/client/overlay/VirtualCursorOverlay.java
+++ common/src/main/java/com/mrcrayfish/controllable/client/overlay/VirtualCursorOverlay.java
@@ -4,13 +4,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mrcrayfish.controllable.Config;
 import com.mrcrayfish.controllable.Controllable;
-import com.mrcrayfish.controllable.client.InputHandler;
 import com.mrcrayfish.controllable.client.settings.CursorStyle;
 import com.mrcrayfish.controllable.client.input.Controller;
-import com.mrcrayfish.controllable.platform.Services;
+import com.mrcrayfish.controllable.platform.ClientServices;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.Slot;
 
 /**
  * Author: MrCrayfish
@@ -34,14 +35,13 @@ public class VirtualCursorOverlay implements IOverlay
         CursorStyle type = Config.CLIENT.options.cursorType.get();
         if(mc.player == null || (mc.player.inventoryMenu.getCarried().isEmpty() || type.getBehaviour() == CursorStyle.ItemHeldBehaviour.SHOW))
         {
-            InputHandler input = Controllable.getInputHandler();
             double guiScale = mc.getWindow().getGuiScale();
             double cursorX = Controllable.getCursor().getRenderX();
             double cursorY = Controllable.getCursor().getRenderY();
             double zIndex = 3000;
             pose.translate(cursorX / guiScale, cursorY / guiScale, zIndex);
             RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            boolean isHoveringSlot = false; // TODO fix
+            boolean isHoveringSlot = this.isHoveringFilledContainerSlot();
             if(isHoveringSlot && type.isScaleHover())
             {
                 pose.scale(1.33F, 1.33F, 1.33F);
@@ -49,5 +49,19 @@ public class VirtualCursorOverlay implements IOverlay
             graphics.blit(CursorStyle.TEXTURE, -8, -8, 16, 16, isHoveringSlot ? 32 : 0, type.ordinal() * 32, 32, 32, 64, CursorStyle.values().length * 32);
         }
         pose.popPose();
+    }
+
+    /**
+     * @return True if the cursor is hovering a container slot
+     */
+    private boolean isHoveringFilledContainerSlot()
+    {
+        Minecraft mc = Minecraft.getInstance();
+        if(mc.screen instanceof AbstractContainerScreen<?> screen)
+        {
+            Slot slot = ClientServices.CLIENT.getSlotUnderMouse(screen);
+            return slot != null && slot.hasItem();
+        }
+        return false;
     }
 }
