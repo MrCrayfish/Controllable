39bb8d72f5f5cb3c94466aee663d43e681b27ae7
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-01-30 22:44:59 +1030

    🐛 Fix crash with optifine

--- forge/src/main/java/com/mrcrayfish/controllable/mixin/client/ForgeGameRendererMixin.java
+++ forge/src/main/java/com/mrcrayfish/controllable/mixin/client/ForgeGameRendererMixin.java
@@ -1,21 +1,18 @@
 package com.mrcrayfish.controllable.mixin.client;
 
-import com.mojang.blaze3d.platform.Window;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mrcrayfish.controllable.Config;
 import com.mrcrayfish.controllable.Controllable;
 import com.mrcrayfish.controllable.client.ControllerInput;
 import com.mrcrayfish.controllable.client.OverlayHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
-import org.joml.Matrix4f;
+import net.minecraft.client.renderer.RenderBuffers;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * Author: MrCrayfish
@@ -23,10 +20,27 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(GameRenderer.class)
 public class ForgeGameRendererMixin
 {
-    @SuppressWarnings("InvalidInjectorMethodSignature")
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;flush()V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void controllableLastRender(float partialTick, long p_109095_, boolean running, CallbackInfo ci, int mouseX, int mouseY, Window window, Matrix4f matrix4f, PoseStack posestack, GuiGraphics graphics)
+    @Shadow
+    @Final
+    Minecraft minecraft;
+
+    @Shadow
+    @Final
+    private RenderBuffers renderBuffers;
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;flush()V"))
+    private void controllableLastRender(float partialTick, long p_109095_, boolean running, CallbackInfo ci)
     {
+        // We don't use locals because of Optifine incompatibility.
+        GuiGraphics graphics = new GuiGraphics(this.minecraft, this.renderBuffers.bufferSource());
+        int mouseX = (int) (this.minecraft.mouseHandler.xpos() * (double) this.minecraft.getWindow().getGuiScaledWidth() / (double) this.minecraft.getWindow().getScreenWidth());
+        int mouseY = (int) (this.minecraft.mouseHandler.ypos() * (double) this.minecraft.getWindow().getGuiScaledHeight() / (double) this.minecraft.getWindow().getScreenHeight());
+        ControllerInput input = Controllable.getInput();
+        if(input.isVirtualCursorActive())
+        {
+            mouseX = (int) input.getScaledCursorX();
+            mouseY = (int) input.getScaledCursorY();
+        }
         OverlayHandler.draw(graphics, mouseX, mouseY, partialTick);
     }
 }
