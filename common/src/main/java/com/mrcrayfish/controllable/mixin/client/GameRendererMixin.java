e2d1fc58814527b7e20a33ae08668a556085fb84
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-02-05 01:20:45 +1030

    📦 Universal approach for mouse pos in screens

--- common/src/main/java/com/mrcrayfish/controllable/mixin/client/GameRendererMixin.java
+++ common/src/main/java/com/mrcrayfish/controllable/mixin/client/GameRendererMixin.java
@@ -1,6 +1,8 @@
 package com.mrcrayfish.controllable.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mrcrayfish.controllable.Controllable;
+import com.mrcrayfish.controllable.client.input.Controller;
 import com.mrcrayfish.controllable.client.overlay.OverlayRenderer;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphics;
@@ -8,6 +10,7 @@ import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -20,5 +23,27 @@ public class GameRendererMixin
     private void controllableLastRender(DeltaTracker tracker, boolean running, CallbackInfo ci, @Local(ordinal = 0) int mouseX, @Local(ordinal = 1) int mouseY, @Local GuiGraphics graphics)
     {
         OverlayRenderer.draw(graphics, mouseX, mouseY, tracker);
+    }
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;clear(IZ)V", remap = false, ordinal = 0), index = 4, ordinal = 0, require = 1)
+    private int controllableModifyMouseX(int original)
+    {
+        Controller controller = Controllable.getController();
+        if(controller != null && controller.isBeingUsed())
+        {
+            return (int) Controllable.getCursor().getRenderScreenX();
+        }
+        return original;
+    }
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;clear(IZ)V", remap = false, ordinal = 0), index = 5, ordinal = 1, require = 1)
+    private int controllableModifyMouseY(int original)
+    {
+        Controller controller = Controllable.getController();
+        if(controller != null && controller.isBeingUsed())
+        {
+            return (int) Controllable.getCursor().getRenderScreenY();
+        }
+        return original;
     }
 }
