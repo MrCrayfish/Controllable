c0d5b4f256b24827aeefb292ce6f1a445c9ec7fc
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-02-17 02:33:03 +1030

    🐛 Fix exp level not affected by console hotbar

--- fabric/src/main/java/com/mrcrayfish/controllable/mixin/client/FabricGuiMixin.java
+++ fabric/src/main/java/com/mrcrayfish/controllable/mixin/client/FabricGuiMixin.java
@@ -33,4 +33,23 @@ public class FabricGuiMixin
             graphics.pose().popPose();
         }
     }
+
+    @Inject(method = "renderExperienceLevel", at = @At(value = "HEAD"))
+    private void consoleExpLevelHead(GuiGraphics graphics, DeltaTracker tracker, CallbackInfo ci)
+    {
+        if(Config.CLIENT.options.consoleHotbar.get())
+        {
+            graphics.pose().pushPose();
+            graphics.pose().translate(0, -25, 0);
+        }
+    }
+
+    @Inject(method = "renderExperienceLevel", at = @At(value = "TAIL"))
+    private void consoleExpLevelTail(GuiGraphics graphics, DeltaTracker tracker, CallbackInfo ci)
+    {
+        if(Config.CLIENT.options.consoleHotbar.get())
+        {
+            graphics.pose().popPose();
+        }
+    }
 }
