c1dc34728c526e87005b5a8d5b4a5e9e56e7d9ad
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-03 14:03:05 +1030

    🐛 Fix crash when using latest JEI

--- common/src/main/java/com/mrcrayfish/controllable/mixin/client/jei/PageNavigationMixin.java
+++ common/src/main/java/com/mrcrayfish/controllable/mixin/client/jei/PageNavigationMixin.java
@@ -1,7 +1,7 @@
 package com.mrcrayfish.controllable.mixin.client.jei;
 
 import mezz.jei.gui.PageNavigation;
-import mezz.jei.gui.elements.GuiIconButton;
+import mezz.jei.gui.elements.IconButton;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -14,8 +14,8 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface PageNavigationMixin
 {
     @Accessor(value = "nextButton", remap = false)
-    GuiIconButton controllableGetNextButton();
+    IconButton controllableGetNextButton();
 
     @Accessor(value = "backButton", remap = false)
-    GuiIconButton controllableGetBackButton();
+    IconButton controllableGetBackButton();
 }
